606769dd866c5c420e36cffe3b43f9a0ad5b4ecd
yulianam30 <yulianam30@hotmail.com>
2015-11-21 12:32:14 -0500

    Creando Splash

---
+++ app/src/main/java/com/example/luisco/ejemploapp/splash.java
@@ -0,0 +1,18 @@
+package com.example.luisco.ejemploapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Luisco on 21/11/2015.
+ */
+public class splash extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+    }
+
+
+
+}
